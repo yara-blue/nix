8452e26031aeb17805cfc580f08ad5ea6491d373
Yara <git@yara.blue>
2026-02-01 16:52:19 +0100

    install rustup as package

--- mixins/build-tools.nix
+++ mixins/build-tools.nix
@@ -9,6 +9,7 @@
 		uv
 		cargo # we generally add flakes to projects and use the cargo from
 		      # there, however we need to have cargo for RA to work in rustc
+		rustup # needed to set stage1 compilers as toolchain for easy testing
 
 		typst
 
